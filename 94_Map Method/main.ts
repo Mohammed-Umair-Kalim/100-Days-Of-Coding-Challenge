041060212d78dfc3c7b2338ad1b03ce13b160dc8
Mohammed-Umair-Kalim <umairkalim@gmail.com>
2024-04-02 03:13:44 -0700

    Q 94 , 95 & 96 completed
    Day 34 challenge from 100Days of coding challenge completed

---
+++ 94_Map Method/main.ts
@@ -0,0 +1,13 @@
+// Question 94: Use the .map() method to create a new array that contains the length of each word from an array
+// of words ?
+
+// Explain & TIP: The .map() method transforms each item in an array according to a specific function and
+// returns a new array with the transformed items.
+
+// Answer 94:
+
+let my_fav_array = ["Pomegranate","Chrlyser Me412","Huawei Mate60 pro"]
+let length_of_each_string = my_fav_array.map(item => item.length)
+
+// it will return 11,14 & 17:
+console.log(length_of_each_string)
---
+++ 95_Filter Method/main.ts
@@ -0,0 +1,13 @@
+// Question 95: Write a function that uses the .filter() method to return an array of numbers greater than 10 ?
+
+// Explain & TIP: The .filter() method creates a new array with all elements that pass the test implemented by
+// the provided function.
+
+// Answer 95:
+let num_array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+function filter(){
+    
+    console.log(num_array.filter(num => num > 10))
+}
+
+filter()
